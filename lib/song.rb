a30562dbc5cd0e9a1e90e8dfd106ca445bba6a22
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 20:19:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Song
 extend Memorable::ClassMethods
-extend Memorable::InstanceMethods
 extend Findable::ClassMethods
+include Memorable::InstanceMethods
 include Paramable
 attr_accessor :name
 attr_reader :artist
