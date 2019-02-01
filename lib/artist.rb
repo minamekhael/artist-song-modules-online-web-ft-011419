5c1a2256cfa15c72a2a28f96406ea718f9304bd1
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 20:00:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,11 +11,7 @@ class Artist
     @@artists << self
     @songs = []
   end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
+  
   def self.all
     @@artists
   end
