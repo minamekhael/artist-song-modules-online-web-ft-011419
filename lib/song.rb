287ae35dc19ee5e726ba3401ab0515fbfb780d80
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 20:16:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,20 @@
 require 'pry'
 
 class Song
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Paramable
-  attr_accessor :name
-  attr_reader :artist
+extend Memorable::ClassMethods
+extend Findable::ClassMethods
+include Paramable
+attr_accessor :name
+attr_reader :artist
 
-    @@songs = []
+@@songs = []
 
   def self.all
     @@songs
   end
-
+  
   def artist=(artist)
     @artist = artist
     artist.add_song(self) unless artist.songs.include?(self)
   end
-
 end
