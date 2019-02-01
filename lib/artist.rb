bd4dc93dbe693ea63361c148a67f72b1bb1ee5c2
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 20:15:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,16 +9,24 @@ class Artist
 
   @@artists = []
 
+    @@artists = []
+  
   def self.all
     @@artists
   end
 
+  def initialize
+    super
+    @songs = []
+  end
+
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
 end
